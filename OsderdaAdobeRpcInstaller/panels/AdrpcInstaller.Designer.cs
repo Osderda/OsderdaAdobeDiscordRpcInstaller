1e8d0dad32ac0b5ec87a1b96a1772725066424a5
Osderda <Osderda>
2022-04-13 16:59:19 +0300

    Bug fixed.

--- OsderdaAdobeRpcInstaller/panels/AdrpcInstaller.Designer.cs
+++ OsderdaAdobeRpcInstaller/panels/AdrpcInstaller.Designer.cs
@@ -52,7 +52,7 @@ namespace OsderdaAdobeRpcInstaller.panels
             // 
             this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
             this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.richTextBox1.ForeColor = System.Drawing.Color.White;
             this.richTextBox1.Location = new System.Drawing.Point(12, 12);
             this.richTextBox1.Name = "richTextBox1";
